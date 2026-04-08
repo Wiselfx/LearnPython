a39adeb53c2f6e02fb62f49f77f5a141379c648b
Jason Grewal <jason.grewal@student.sl.on.ca>
2026-04-08 16:07:01 +0200

    Update Report_Card_printer.py

--- ReportCardPrinter/Report_Card_printer.py
+++ ReportCardPrinter/Report_Card_printer.py
@@ -1,1 +1,11 @@
+student_name='Alice'
+print(student_name,(type(student_name))
 
+is_student=True
+print(is_student,type(is_student))
+
+student_age=18
+print(student_age,type(student_age))
+
+score=92.5
+print(isinstance(score, int))
