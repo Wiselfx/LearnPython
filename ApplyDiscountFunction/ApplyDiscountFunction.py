aad39570862d1f1e93f812f5e5220c850ca108d5
Wiselfx <jason.grewal@student.sl.on.ca>
2026-04-12 23:39:16 +0200

    Discount Function for the lab:
        You should define a function named apply_discount.
    
        The apply_discount function should take exactly two parameters: price and discount.
    
        If price is not a number (int or float), the function should return the string The price should be a number.
    
        If discount is not a number (int or float), the function should return the string The discount should be a number.
    
        If price is less than or equal to 0, the function should return the string The price should be greater than 0.
    
        If discount is less than 0 or greater than 100, the function should return the string The discount should be between 0 and 100.
    
        If both inputs are valid, the function should calculate the discount as a percentage of the price.
    
        The function should return the final price after applying the discount.

---
+++ ApplyDiscountFunction/ApplyDiscountFunction.py
@@ -0,0 +1,11 @@
+def apply_discount(price, discount):
+    if not isinstance(price,(int,float)):
+        return 'The price should be a number'
+    if not isinstance(discount,(int,float)):
+        return 'The discount should be a number'
+    if price <=0:
+        return 'The price should be greater than 0'
+    if 100 < discount or discount < 0:
+        return 'The discount should be between 0 and 100'
+    return price * ( 1 - discount / 100)
+print(apply_discount(50,6))
